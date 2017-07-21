cc9afcb45873025e188c27b3739f5f8477f807e7
wt3639 <wutao3639@163.com>
2017-07-22 01:07:44 +0800

    first

---
+++ src/main/java/com/tom/counterwebapp/controller/GeneralController.java
@@ -0,0 +1,22 @@
+package com.tom.counterwebapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class GeneralController {
+
+@RequestMapping(value="index.do")  
+
+    public void index_jsp(Model model){  
+
+        model.addAttribute("hello", "Hi,SpringMVC");  
+
+        System.out.println("index.jsp");  
+
+    }
+}
