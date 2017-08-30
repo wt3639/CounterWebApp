0e8633b31030691cb77ec76660ca0dc26e81f6a0
wt3639 <wutao3639@163.com>
2017-08-30 18:22:18 +0800

    add getjson

---
+++ src/main/java/com/tom/counterwebapp/bean/RespondResult.java
@@ -0,0 +1,46 @@
+package com.tom.counterwebapp.bean;
+
+public class RespondResult {
+	  private int basic; 
+	  private int total; 
+	  private int need; 
+	  private int prot;
+	  private int fat; 
+	  private int cab;
+	public int getBasic() {
+		return basic;
+	}
+	public void setBasic(int basic) {
+		this.basic = basic;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getNeed() {
+		return need;
+	}
+	public void setNeed(int need) {
+		this.need = need;
+	}
+	public int getProt() {
+		return prot;
+	}
+	public void setProt(int prot) {
+		this.prot = prot;
+	}
+	public int getFat() {
+		return fat;
+	}
+	public void setFat(int fat) {
+		this.fat = fat;
+	}
+	public int getCab() {
+		return cab;
+	}
+	public void setCab(int cab) {
+		this.cab = cab;
+	} 
+}
--- src/main/java/com/tom/counterwebapp/controller/CaloryController.java
+++ src/main/java/com/tom/counterwebapp/controller/CaloryController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tom.counterwebapp.bean.RespondResult;
 import com.tom.counterwebapp.bean.Student;
 import com.tom.counterwebapp.service.IStudentService;
 
@@ -69,5 +71,60 @@ public class CaloryController {
 	        model.addAttribute("fat", fat); 
 	        model.addAttribute("cab", cab); 
 	        return "calResult";  
+	    }
+	    
+	    @RequestMapping(value="/getjson",method={RequestMethod.GET})  
+	    @ResponseBody
+	    public RespondResult toJson(HttpServletRequest request) throws Exception{  
+	    	RespondResult rr = new RespondResult();
+	        String sex = request.getParameter("Sex");
+	        String height = request.getParameter("height");
+	        String weight = request.getParameter("weight");
+	        String age = request.getParameter("age");
+	        String sportIndex = request.getParameter("sportIndex");
+	        String aerobic = request.getParameter("aerobic");
+	        String goal = request.getParameter("Goal");
+	        String energy = request.getParameter("energy");
+	        Student student = new Student();
+	        student.setName(sex);
+	        student.setQq(height);
+	        student.setLink(weight);
+	        student.setSchool(age);
+	        student.setSource(sportIndex);
+	        student.setStId(aerobic);
+	        student.settA(goal);
+	        student.setType(energy);
+	        studentService.addStudent(student);
+	        Integer basic =null;
+	        Integer need=null;
+	        Integer prot =null;
+	        Integer fat =null;
+	        Integer cab =null;
+	        if(sex.equals("male")){
+	         
+	             basic = (int) (90 + 4.8* Integer.parseInt(height) + 13.4* Integer.parseInt(weight) - 5.7* Integer.parseInt(age));
+	        }
+	        else {
+	             basic = (int) ( 450 + 3.1* Integer.parseInt(height) + 9.2* Integer.parseInt(weight) - 4.3* Integer.parseInt(age));      
+	        }	       
+	        Integer total =(int) (basic * Double.parseDouble(sportIndex)+Integer.parseInt(aerobic));	         
+	            if(goal.equals("muscle")){
+	                 need = ( total + Integer.parseInt(energy));
+	                 prot =(int) ( 2.2 * Integer.parseInt(weight));
+	                 fat =  (int) (need * 0.25 / 9 );
+	                 cab =  ((need - prot*4 -fat*9)/4);
+	            }else{
+	                 need =  ( total - Integer.parseInt(energy));
+	                 prot = (int) ( 2.75 * Integer.parseInt(weight));
+	                 fat = (int) ( need * 0.2 / 9) ;
+	                 cab =  ((need - prot*4 -fat*9)/4);
+	               }                
+	        rr.setBasic(basic);  	        
+	        rr.setCab(cab);
+	        rr.setFat(fat);
+	        rr.setNeed(need);
+	        rr.setProt(prot);
+	        rr.setTotal(total);
+	        return rr;  
 	    }
 }
